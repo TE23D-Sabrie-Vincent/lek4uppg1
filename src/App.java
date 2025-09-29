16fd8e800f80a099e8d299f8ee797fcdacc54c17
Vincent Sabrie <vincent.sabrie@elev.ga.ntig.se>
2025-09-29 14:15:27 +0200

    Commit

---
+++ src/App.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int val = 0;
+
+
+        boolean loop = true;
+        while (loop){
+            System.out.println("\n Meny:");
+            System.out.println("Lägg till elev info");
+            System.out.println("2. Visa alla elevernas information (lol din creep)");
+            System.out.println("3. Avlsut program");
+        }
+        if (val == 3) {
+            
+        }
+
+    }
+}
---
+++ src/Elev.java
@@ -0,0 +1,20 @@
+public class Elev {
+    String namn;
+    String personnummer;
+    String email;
+    String mobilnummer;
+    String klass;
+    String program;
+
+    public Elev(String namn, String personnummer,String email, 
+    String mobilnummer, String klass, String program){
+
+    this.namn=namn;
+    this.personnummer = personnummer;
+    this.email = email;
+    this.mobilnummer = mobilnummer;
+    this.klass = klass;
+    this.program = program;
+
+    }
+}
