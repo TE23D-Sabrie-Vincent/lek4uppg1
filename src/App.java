7aaf5f7c6a5d2ca82c77b932232ac0517865996f
Vincent Sabrie <vincent.sabrie@elev.ga.ntig.se>
2025-09-30 12:46:09 +0200

    klart tror jag.

--- src/App.java
+++ src/App.java
@@ -1,21 +1,86 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        int val = 0;
-
-
+        ArrayList<Elev> elever = new ArrayList<>();
         boolean loop = true;
-        while (loop){
+
+        while (loop) {
             System.out.println("\n Meny:");
             System.out.println("Lägg till elev info");
             System.out.println("2. Visa alla elevernas information (lol din creep)");
             System.out.println("3. Avlsut program");
-        }
-        if (val == 3) {
-            
-        }
 
+            int val;
+            try {
+                val = scanner.nextInt();
+            } catch (Exception e) {
+                System.out.println(" Du måste skriva ett nummer (1-3), eller så gick något annat fel.");
+                scanner.nextLine();
+                continue;
+            }
+
+            if (val == 3) {
+                System.out.println("Programmet stängs ned");
+                loop = false;
+            }
+
+            if (val == 1){
+                System.out.println("Ditt namn:");
+                String namn =  scanner.nextLine();
+
+                System.out.println("Vad är ditt personNum? (ÅÅÅÅMMDD)");
+                String personNum = scanner.nextLine();
+
+                try {
+                    personNum = scanner.nextLine();
+                }
+                catch (Exception e){
+                    System.out.println("Du måste skriva ett nummer för platsen");
+                    scanner.nextLine();
+                    continue;
+                }   
+
+                if (personNum.length() != 8) {
+                    System.out.println("Ogiltigt personNum, det ska finnas 8 nummer (ÅÅÅÅMMDD)");
+                    continue;
+                }
+
+                System.out.println("Din mail:");
+                String mail = scanner.nextLine();
+
+                System.out.println("Din mobil nummer");
+                String mobilNum = scanner.nextLine();
+
+                System.out.println("Din klass:");
+                String klass = scanner.nextLine();
+
+                System.out.println("Ditt program:");
+                String program = scanner.nextLine();
+
+                Elev nyElev = new Elev(namn, personNum, mail, mobilNum, klass, program);
+                elever.add(nyElev);
+
+                System.out.println("Eleven är registrerad");
+            }
+            else if (val == 2) {
+                if (elever.isEmpty()) {
+                    System.out.println("Det finns inga elever registrerade lmao :P");
+                }
+                else{
+                    System.out.println("\n Alla eleverna:");
+                    for (Elev e : elever){
+                        e.skrivUtInfo();
+                    }
+                }
+            }
+            else{
+                System.out.println("Ogiltigt val, välj mellan 1-3");
+                continue;
+            }
+        }
+        scanner.close();
     }
 }
--- src/Elev.java
+++ src/Elev.java
@@ -1,20 +1,30 @@
 public class Elev {
     String namn;
-    String personnummer;
+    String personNum;
     String email;
-    String mobilnummer;
+    String mobilNum;
     String klass;
     String program;
 
-    public Elev(String namn, String personnummer,String email, 
-    String mobilnummer, String klass, String program){
+    public Elev(String namn, String personNum,String email, 
+    String mobilNum, String klass, String program){
 
     this.namn=namn;
-    this.personnummer = personnummer;
+    this.personNum = personNum;
     this.email = email;
-    this.mobilnummer = mobilnummer;
+    this.mobilNum = mobilNum;
     this.klass = klass;
     this.program = program;
 
+    }
+
+    public void skrivUtInfo() {
+        System.out.println("\n Elev registrerad");
+        System.out.println("Namn: " + namn);
+        System.out.println("personNum: " + personNum);
+        System.out.println("Email: " + email);
+        System.out.println("mobilNum: " + mobilNum);
+        System.out.println("Klass: " + klass);
+        System.out.println("Program: " + program);
     }
 }
